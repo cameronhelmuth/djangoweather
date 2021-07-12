f16cffda89092853503de6f8b34a7b155a2ee7b5
Cameron Helmuth <emailme@cameronhelmuth.com>
2021-07-12 13:13:43 -0400

    stuck2

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+    
+    api_requests = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=100&API_KEY=0F0131C0-5F0D-476F-B8FA-8445F52CE76C")
+    
+    try:
+        api = json.loads(api_requests.content)
+        
+    except Exception as e:
+        api = "Error..."
+    
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
